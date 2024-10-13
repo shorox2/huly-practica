8ae9f69e033fade2809f762c9f8ce26ead9e5193
shorox <85540487+shorox2@users.noreply.github.com>
2024-10-13 23:46:21 +0300

    Add files via upload

---
+++ huly-selfhost-main/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export SERVER_ADDRESS="$1"
+export HULY_SECRET="secret"
+
+echo "Setting Huly Server Address: $SERVER_ADDRESS"
+envsubst < template.conf > nginx.conf
+envsubst < template.env > .env
+
+./use-version.sh v0.6.313
